bc0da4148fcab368c2ee40648a1de07501c893e8
Daniel Dowler <shaolinsamurai1988@gmail.com>
2018-01-05 18:38:52 -0700

    tag work

--- tagrouter.js
+++ tagrouter.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const router= express.Router();
 const Tag= require('./models/tag');
+const image = require('./models/image')
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
@@ -13,7 +14,22 @@ Tag.find().then(tags => {
 });
 
 router.post('/', (req,res) =>{
-    Tag.create(req.body).then(tag => res.json(tag));
+    let new_tag = {
+        name: req.body.name,
+        date: Date.now()
+    };
+    Tag
+        .create(new_tag)
+        .then(tag => {
+            console.log('This tag was saved', tag);
+            Image.findById(req.body.image_id, (err, image) => {
+              console.log('This image was loaded', image);
+              image.tags ? image.tags.push(tag) : image.tags = [tag];
+              image
+                  .save(() => res.json(tag));
+            });
+
+        });
 });
 
 router.put('/', (req,res) =>{
